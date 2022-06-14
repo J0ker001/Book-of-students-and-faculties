a6e650281af52809cc1e1b9864202c0d267256a3
Anton <96440353+J0ker001@users.noreply.github.com>
2022-06-14 22:43:32 +0300

    Update scripts422.sql

--- scripts422.sql
+++ scripts422.sql
@@ -1,2 +1,3 @@
+-- создание таблиц cars и people и связывание их
 CREATE TABLE cars (id INTEGER PRIMARY KEY , name TEXT, model TEXT, price INTEGER);
 CREATE TABLE people ( id INTEGER, name  TEXT, age INTEGER, drivingLicense BOOLEAN, cars_id INTEGER REFERENCES cars (id) );
