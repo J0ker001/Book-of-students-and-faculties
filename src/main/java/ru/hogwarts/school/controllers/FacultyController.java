d7cb5d1e839725023b87d27015cff63af52fbc5c
Anton <96440353+J0ker001@users.noreply.github.com>
2022-06-14 23:13:54 +0300

    Update FacultyController.java

--- src/main/java/ru/hogwarts/school/controllers/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controllers/FacultyController.java
@@ -20,12 +20,14 @@ public class FacultyController {
         this.facultyService = facultyService;
     }
 
+//создать факультет    
     @PostMapping("/createNew")
     public ResponseEntity<Faculty> creatFaculty(@RequestBody Faculty faculty) {
         Faculty creatFaculty = facultyService.createFaculty(faculty);
         return ResponseEntity.ok(creatFaculty);
     }
 
+//найти факультет по id    
     @GetMapping("/find/{id}")
     public ResponseEntity<Faculty> getFaculty(@PathVariable Long id) {
         Faculty faculty = facultyService.findFaculty(id);
@@ -35,34 +37,40 @@ public class FacultyController {
         return ResponseEntity.ok(faculty);
     }
 
+//обновить факультет    
     @PutMapping("/update")
     public ResponseEntity<Faculty> editFaculty(@RequestBody Faculty faculty) {
         Faculty editFaculty = facultyService.editFaculty(faculty);
         return ResponseEntity.ok(editFaculty);
     }
 
+//удалить факультет по id    
     @DeleteMapping("/delete/{id}")
     public ResponseEntity deleteFaculty(@PathVariable Long id) {
         facultyService.deleteFaculty(id);
         return ResponseEntity.ok().build();
     }
 
+//отсортировать факультет по цвету    
     @GetMapping("filter/{color}")
     public List<Faculty> filteredColor(@PathVariable String color) {
         return facultyService.sortFaculties(color);
     }
-
+    
+//список факультетов по имени или цвету    
     @GetMapping("findNAmeORColor")
     public Collection<Faculty> findByNameOrColor(@RequestParam(required = false) String name,
                                                  @RequestParam(required = false) String color) {
         return facultyService.findByNameOrColor(name, color);
     }
 
+//список студентов по названию факультета    
     @GetMapping("search")
     public Collection<Student> findByName(@RequestParam String name) {
         return facultyService.findByName(name);
     }
 
+// самое большое название факультета    
     @GetMapping("/longestFacultyName")
     public String longestFacultyName(){
       return   facultyService.longestFacultyName();
